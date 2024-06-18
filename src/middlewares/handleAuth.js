1af8dfd33011ec9c37ea319fb9e1b9af483b6e04
ivan franca <contato.ivanfrancajr@gmail.com>
2024-06-18 14:51:53 -0300

    fix/ fix small issue for handle with invalid tokens

--- src/middlewares/handleAuth.js
+++ src/middlewares/handleAuth.js
@@ -4,8 +4,6 @@ import { users } from "../controllers/users.controllers.js";
 export const handleAuth = async (req, res, next) => {
   const authHeader = req.headers["authorization"];
 
-  console.log(req.headers);
-
   const bearer = authHeader && authHeader.split(" ")[1];
 
   if (!bearer) {
@@ -15,7 +13,11 @@ export const handleAuth = async (req, res, next) => {
   try {
     const is_valid = await jwt.verify(bearer, "janela");
 
-    req.user = await users.find((user) => user.id === is_valid.id);
+    const user = await users.find((user) => user.id === is_valid.id);
+
+    if (!user) return res.status(401).json({ errors: ["Not authorized"] });
+
+    req.user = user;
 
     next();
   } catch (error) {
