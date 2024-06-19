d19f58a6d8a6420b4642eac76a9783b674e1a75a
ivan franca <contato.ivanfrancajr@gmail.com>
2024-06-19 14:52:19 -0300

    refact/ refact calculateAmout function

--- src/utils/calculateAmount.js
+++ src/utils/calculateAmount.js
@@ -1,13 +1,18 @@
-export const calculateAmount = (transactionList) => {
-  const result = transactionList.reduce((balance, transaction) => {
+import { Transaction } from "../models/Transaction.js";
+
+const transactionList = await Transaction.find();
+
+export const calculateAmount = async (items) => {
+  const result = items.reduce((balance, transaction) => {
     if (transaction.type === "income") {
       balance += transaction.amount;
     } else if (transaction.type === "expense") {
       balance -= transaction.amount;
     }
-
     return balance;
   }, 0);
 
   return result;
 };
+
+calculateAmount(transactionList);
